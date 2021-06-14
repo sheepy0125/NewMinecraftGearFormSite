b8bd8509f9adb16f1a569346e1343b79704f7d8b
sheepy0125 <sheepy404@gmail.com>
2021-06-14 16:17:45 -0400

    Set up everything

---
+++ api/api.py
@@ -0,0 +1,20 @@
+"""
+API for god gear ordering site
+Created on 06/14/2021
+"""
+
+""" Setup """
+from os import stat
+from flask import Flask
+from json import load
+
+api = Flask(__name__, template_folder=None, static_folder="static")
+
+with open("json_files/config.json") as config_file:
+	config_dict = load(config_file)
+
+""" Routes """
+# Ping
+@api.route("/ping", methods=["GET", "POST"])
+def ping_route() -> dict[str, bool]:
+	return {"worked": True}
---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+import {Component} from "react";
+
+export default class Main extends Component {
+	render = () => {
+		return <p className="container py-2 mx-auto my-2 font-mono text-lg text-center bg-blue-300 shadow-lg">Nothing here yet!</p>;
+	};
+}
