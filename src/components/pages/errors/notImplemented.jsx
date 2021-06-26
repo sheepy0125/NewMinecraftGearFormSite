11d7103c8a452af0d08e78563d189e660c1eb1c6
sheepy0125 <sheepy404@gmail.com>
2021-06-26 13:33:24 -0400

    why did vscode git not commit these files? lol

--- src/components/pages/errors/notImplemented.jsx
+++ src/components/pages/errors/notImplemented.jsx
@@ -1,17 +1,17 @@
 // Not implemented error page
 
-import ErrorWidget from "../../boilerplate/widgets/boxWidget.jsx";
+import BoxWidget from "../../boilerplate/widgets/boxWidget.jsx";
 import Button from "../../boilerplate/button.jsx";
 import {Link} from "react-router-dom";
 
 export default function NotImplemented() {
 	return (
-		<ErrorWidget message="This hasn't been implemented.">
+		<BoxWidget message="This hasn't been implemented.">
 			<Link to="/home">
 				<Button normalColor="red-300" activeColor="red-400" className="w-full mx-auto">
 					Return home
 				</Button>
 			</Link>
-		</ErrorWidget>
+		</BoxWidget>
 	);
 }
