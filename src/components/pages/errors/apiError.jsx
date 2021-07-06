eac1d43578eadb11b89a7112d27ae4bdcc82aa9c
sheepy0125 <sheepy404@gmail.com>
2021-07-06 16:17:39 -0400

    Reword API error page

--- src/components/pages/errors/apiError.jsx
+++ src/components/pages/errors/apiError.jsx
@@ -9,8 +9,9 @@ export default function ApiError() {
 		<BoxWidget message="There was an error with the API.">
 			<div className="text-xl">
 				<p className="font-normal">This is probably not good.</p>
+				<p className="font-thin">You can check your browser console to see if there are any error messages.</p>
 				<p className="font-normal">
-					If you weren't trying to break this, please{" "}
+					You may try again of what you were doing, but you should probably{" "}
 					<Link to="/not-implemented" className="text-blue-900 cursor-pointer hover:underline">
 						contact Sheepy
 					</Link>
