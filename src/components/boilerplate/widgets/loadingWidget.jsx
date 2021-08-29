5c32dd0f58f8a3b34bcf1355e60c8243065c4be8
sheepy0125 <sheepy404@gmail.com>
2021-08-29 14:20:36 -0400

    Fix widgets CSS

--- src/components/boilerplate/widgets/loadingWidget.jsx
+++ src/components/boilerplate/widgets/loadingWidget.jsx
@@ -4,7 +4,7 @@ import BaseWidget from "./baseWidget.jsx";
 
 export default function LoadingWidget(props) {
 	return (
-		<BaseWidget className="flex mx-auto max-w-max">
+		<BaseWidget className="flex mx-auto lg:mx-auto max-w-max">
 			{/* This loading animation totally wasn't stolen from the TailwindCSS docs... */}
 			<svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 				<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
