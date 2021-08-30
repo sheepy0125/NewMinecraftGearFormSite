86324c7d1935b65c114b5a9f804a3b23a4ddaab9
sheepy0125 <sheepy404@gmail.com>
2021-08-30 17:37:42 -0400

    A couple of bugs fixed

--- src/components/boilerplate/widgets/boxWidget.jsx
+++ src/components/boilerplate/widgets/boxWidget.jsx
@@ -4,7 +4,7 @@ import BaseWidget from "./baseWidget.jsx";
 
 export default function BoxWidget(props) {
 	return (
-		<BaseWidget className={"max-w-max m-4 xl:mx-auto" /* responsive design classes take priority */}>
+		<BaseWidget className={"max-w-max mx-auto lg:mx-auto"}>
 			<p className="text-3xl">{props.message}</p>
 			{props.children}
 		</BaseWidget>
--- src/components/boilerplate/widgets/listWidget.jsx
+++ src/components/boilerplate/widgets/listWidget.jsx
@@ -4,7 +4,7 @@ import BaseWidget from "./baseWidget.jsx";
 
 export default function ListWidget(props) {
 	return (
-		<BaseWidget className={`lg:px-8 lg:py-2 text-left list-disc bg-gray-200 ${props.className}`} activeOpacity="80" inactiveOpacity="70">
+		<BaseWidget className={`px-8 lg:py-2 lg:px-10 text-left list-disc bg-gray-200 ${props.className}`} activeOpacity="80" inactiveOpacity="70">
 			{props.children}
 		</BaseWidget>
 	);
